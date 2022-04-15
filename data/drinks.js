8a32bfa46fbec4e2b529fbc090f8b5a2660a99c2
codesquadkiki <47373509+codesquadkiki@users.noreply.github.com>
2022-04-15 15:41:08 -0400

    initial commit

---
+++ data/drinks.js
@@ -0,0 +1,27 @@
+[
+  {
+    item: "Coffee",
+    description: "Regular coffee",
+    price: "2.50"
+  },
+  {
+    item: "Chocolato",
+    description: "Chocolate espresso with milk",
+    price: "4.50"
+  },
+  {
+    item: "Corretto",
+    description: "Whiskey and coffee",
+    price: "5.00"
+  },
+  {
+    item: "Iced tea",
+    description: "Hot tea, except not hot",
+    price: "3.00"
+  },
+  {
+    item: "Soda",
+    description: "Coke, Sprite, Fanta, etc",
+    price: "2.50"
+  },
+]
